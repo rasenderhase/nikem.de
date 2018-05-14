5bf1104ee8b677735f5c2c432977bb8dbb0cf9a2
Andreas Knees <andreas.knees@nikem.de>
2018-05-15 00:07:37 +0200

    add hero

---
+++ src/js/site.js
@@ -0,0 +1,16 @@
+(function(window) {
+    var tmpf = window.onload;
+
+    window.onload = function () {
+        if (tmpf) tmpf();
+
+        document.body.onclick = function (evt) {
+            var target = evt.originalTarget,
+            toggle = document.getElementById("toggle");
+            if (target.id != toggle.id && target.htmlFor != toggle.id) {
+                toggle.checked = false;
+            }
+        }
+
+    }
+}) (window);
