e77f57472d52ac41651b065e2d9179a34370af8c
Andreas Knees <andreas.knees@nikem.de>
2018-05-16 13:44:43 +0200

    click on iphone

--- src/js/site.js
+++ src/js/site.js
@@ -1,16 +1,16 @@
 (function(window) {
-    var tmpf = window.onload;
-
-    window.onload = function () {
-        if (tmpf) tmpf();
-
-        document.body.onclick = function (evt) {
+    var tmpf = window.onload,
+        clickHandler = function (evt) {
             var target = evt.originalTarget || evt.target,
-            toggle = document.getElementById("toggle");
+                toggle = document.getElementById("toggle");
             if (target.id != toggle.id && target.htmlFor != toggle.id) {
                 toggle.checked = false;
             }
-        }
+        };
 
+    window.onload = function () {
+        if (tmpf) tmpf();
+        document.getElementById("navigation").addEventListener("click", clickHandler, false);
+        document.getElementById("page").addEventListener("click", clickHandler, false);
     }
 }) (window);
